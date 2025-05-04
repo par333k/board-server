ef7eb2c8028c90ea7783bde69555abc8c5be8eef
jinwoongpark <par333k@naver.com>
2025-05-04 20:50:19 +0900

    feat: 풀텍스트 인덱스 추가

--- board-server.sql
+++ board-server.sql
@@ -37,7 +37,6 @@ CREATE TABLE IF NOT EXISTS keywords (
 
 ALTER TABLE keywords
 ADD FULLTEXT INDEX idx_keyword_fulltext (keyword);
-
 -- 기본 알림 정보
 CREATE TABLE IF NOT EXISTS notifications (
     id INT AUTO_INCREMENT PRIMARY KEY,
