2eec5966fc391f154c78ae7e176d7962f831aa54
jinwoong.park <par333k@naver.com>
2025-05-04 20:23:53 +0900

    feat: 의존성 추가

--- src/keywords/keywords.module.ts
+++ src/keywords/keywords.module.ts
@@ -3,9 +3,10 @@ import { KeywordEntity } from './entities/keyword.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { KeywordsService } from './keywords.service';
 import { KeywordsRepository } from './keywords.repository';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([KeywordEntity])],
+  imports: [TypeOrmModule.forFeature([KeywordEntity]), CacheModule],
   providers: [KeywordsService, KeywordsRepository],
   exports: [KeywordsService, KeywordsRepository],
 })
